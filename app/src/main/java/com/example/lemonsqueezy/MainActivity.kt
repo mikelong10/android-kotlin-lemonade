04d74fcaa3b6056382d4d508890a1b6c938e9a5d
Michael Long <michaeltlong10@gmail.com>
2024-10-16 17:09:22 -0700

    create app layout

--- app/src/main/java/com/example/lemonsqueezy/MainActivity.kt
+++ app/src/main/java/com/example/lemonsqueezy/MainActivity.kt
@@ -4,19 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,6 +60,7 @@ fun LemonSqueezyApp() {
 fun LemonSqueezyContent(modifier: Modifier = Modifier) {
     Column(modifier = modifier.fillMaxSize()) {
         LemonSqueezyTitleBar()
+        LemonSqueezyInteractiveArea()
     }
 }
 
@@ -59,7 +70,7 @@ fun LemonSqueezyTitleBar() {
         text = "Lemonade",
         modifier = Modifier
             .padding()
-            .background(color = Color.Yellow)
+            .background(color = Color(0xFFF8E44C))
             .fillMaxWidth()
             .height(64.dp)
             .wrapContentSize(Alignment.Center),
@@ -68,3 +79,29 @@ fun LemonSqueezyTitleBar() {
     )
 }
 
+@Composable
+fun LemonSqueezyInteractiveArea() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button (
+            onClick = {},
+            shape = RoundedCornerShape(dimensionResource(R.dimen.image_button_border_radius)),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFC3ECD2)),
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painterResource(R.drawable.lemon_tree),
+                contentDescription = "Lemonade stage",
+                modifier = Modifier
+                    .height(dimensionResource(R.dimen.image_button_height))
+                    .width(dimensionResource(R.dimen.image_button_width))
+                    .padding(dimensionResource(R.dimen.image_button_padding))
+            )
+        }
+        Text(text = "Tap the lemon tree to select a lemon")
+    }
+}
